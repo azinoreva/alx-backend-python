c0dcc5533df5c3126f5772c0afcac7e043e58022
azinoreva <azinoreva@gmail.com>
2024-08-20 03:41:42 +0000

    done

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,9 @@ Asynchronous coroutine that waits for a random delay
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10):
+    """ Returns random delay of function process"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
